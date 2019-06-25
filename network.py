b98b5a3a572384c7543bb502b302b60b943cc0e4
Tech With Tim <mrtechguy234@gmail.com>
2019-06-25 19:05:53 +0000

    changed server ip to localhost

--- network.py
+++ network.py
@@ -5,10 +5,10 @@ class Network:
 
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "10.11.250.207" # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
+        self.host = "localhost" # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
                                     # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
                                     # ipv4 address. This feild will be the same for all your clients.
-        self.port = 5555            
+        self.port = 5555
         self.addr = (self.host, self.port)
         self.id = self.connect()
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server = ''
+server = 'localhost'
 port = 5555
 
 server_ip = socket.gethostbyname(server)
